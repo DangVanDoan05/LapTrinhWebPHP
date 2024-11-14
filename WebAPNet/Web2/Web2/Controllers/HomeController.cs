9407c027fa75a242a3ab1d0c582254602bdea52f
Dang Van Doan 05 <dangvandoan0509@gmail.com>
2024-11-14 17:25:53 +0700

    update 1 14-11-2024

---
+++ WebAPNet/Web2/Web2/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web2.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        // GET: Home
+        // Luôn chạy File Index đầu tiên.
+        public ActionResult Trangchu()
+        {
+            // Chuyển hướng qua action khác , cùng controller.
+            // return RedirectToAction("DangNhap");
+            // 2.Chuyển hướng khác controller
+            //  return RedirectToAction("DanhSachTinTuc", "TinTuc");
+            // 3.Chuyển hướng tới 1 Url
+           
+            // 4.
+            return View();
+        }
+
+        public ActionResult GioiThieu()
+        {
+            return View();
+        }
+
+        public ActionResult DangNhap()
+        {
+            return View();
+        }
+     
+    }
+}
---
+++ WebAPNet/Web2/Web2/Controllers/TinTucController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web2.Controllers
+{
+    public class TinTucController : Controller
+    {
+        // GET: TinTuc
+        public ActionResult DanhSachTinTuc()
+        {
+            return View();
+        }
+
+        public ActionResult NoiDungTin()
+        {
+            return View();
+        }
+
+    }
+}
